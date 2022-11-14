55564e2aa52597ef2908dd0807577077eda4cfc5
Nath <nathinabob@gmail.com>
2022-11-14 15:08:26 +0000

    feat: add share button to custom map

--- pages/pray/action-global-map.php
+++ pages/pray/action-global-map.php
@@ -129,11 +129,11 @@ class PG_Global_Prayer_App_Map extends PG_Global_Prayer_App {
                     <div class="map-overlay" id="map-legend"></div>
                     <div class="row">
                         <div class="col col-12 center"><button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas_stats"><i class="ion-chevron-up two-em"></i></button></div>
-                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Remaining</strong><br><strong><span class="one-em red stats-figure remaining"></span></strong></div>
-                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Covered</strong><br><strong><span class="one-em green stats-figure completed"></span></strong></div>
-                        <div class="col col-sm-6 col-md-3 center"><strong>Prayer Warriors</strong><br><img class="three-em" style="padding-top:5px;" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/praying-hand-up-20.png' ) ?>" /></div>
-                        <div class="col col-sm-6 col-md-2 center"><strong>Your Recent Prayers</strong><br><img class="three-em" style="padding-top:5px;" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/black-check-50.png' ) ?>" /></div>
-                        <div class="col col-sm-1 col-md-1 center">
+                        <div class="col col-6 col-sm-3 center d-none d-sm-block"><strong>Places Remaining</strong><br><strong><span class="one-em red stats-figure remaining"></span></strong></div>
+                        <div class="col col-6 col-sm-3 center d-none d-sm-block"><strong>Places Covered</strong><br><strong><span class="one-em green stats-figure completed"></span></strong></div>
+                        <div class="col col-6 col-sm-3 center"><strong>Prayer Warriors</strong><br><img class="three-em" style="padding-top:5px;" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/praying-hand-up-20.png' ) ?>" /></div>
+                        <div class="col col-6 col-sm-2 center"><strong>Your Recent Prayers</strong><br><img class="three-em" style="padding-top:5px;" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/black-check-50.png' ) ?>" /></div>
+                        <div class="col col-12 col-sm-1 text-end my-auto">
                             <img class="icon-button share-button" data-toggle="modal" data-target="#exampleModal" src="<?php echo esc_html( plugin_dir_url( __DIR__ ) ) ?>/assets/images/share.svg" alt="Share">
                         </div>
                     </div>
