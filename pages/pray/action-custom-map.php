4fd8bac56112524f01a9238f54f3625d039dbd38
Chris Chasm <chris@chasm.solutions>
2022-11-25 18:49:08 -0700

    Fix https://github.com/Pray4Movement/prayer-global-porch/issues/73

--- pages/pray/action-custom-map.php
+++ pages/pray/action-custom-map.php
@@ -134,8 +134,8 @@ class PG_Custom_Prayer_App_Map extends PG_Custom_Prayer_App {
                     <div class="row">
                         <div class="col col-12 center"><button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas_stats" aria-controls="offcanvas_stats"><i class="ion-chevron-up two-em"></i></button></div>
 
-                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Remaining</strong><br><strong><span class="one-em red stats-figure remaining"></span></strong></div>
-                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Covered</strong><br><strong><span class="one-em green stats-figure completed"></span></strong></div>
+                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Remaining</strong><br><strong><span class="one-em red-bg stats-figure remaining"></span></strong></div>
+                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Covered</strong><br><strong><span class="one-em green-bg stats-figure completed"></span></strong></div>
                          <div class="col col-sm-6 col-md-3 center">
                             <strong>Prayer Warriors</strong>
                             <br>
--- pages/pray/action-global-map.php
+++ pages/pray/action-global-map.php
@@ -132,8 +132,8 @@ class PG_Global_Prayer_App_Map extends PG_Global_Prayer_App {
                     <div class="map-overlay" id="map-legend"></div>
                     <div class="row">
                         <div class="col col-12 center"><button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas_stats"><i class="ion-chevron-up two-em"></i></button></div>
-                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Remaining</strong><br><strong><span class="one-em red stats-figure remaining"></span></strong></div>
-                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Covered</strong><br><strong><span class="one-em green stats-figure completed"></span></strong></div>
+                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Remaining</strong><br><strong><span class="one-em red-bg stats-figure remaining"></span></strong></div>
+                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Covered</strong><br><strong><span class="one-em green-bg stats-figure completed"></span></strong></div>
                         <div class="col col-sm-6 col-md-3 center">
                             <strong>Prayer Warriors</strong>
                             <br>
