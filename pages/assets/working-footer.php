9bace0944c934052234e47915c9657a1ac95de04
Chris Chasm <chris@chasm.solutions>
2023-01-14 15:48:02 -0700

    Update working-footer.php

--- pages/assets/working-footer.php
+++ pages/assets/working-footer.php
@@ -18,8 +18,6 @@
                 <ul style="list-style: none;">
                     <li><a class="white" href="/prayer_app/contact_us/">Contact Us</a></li>
                     <li><a class="white" href="https://gospelambition.org/donate/">Donate</a></li>
-
-                    <li><a class="white" href="/prayer_app/subscribe/">Subscribe for News</a></li>
                 </ul>
             </div>
             <div class="col-md-3">
