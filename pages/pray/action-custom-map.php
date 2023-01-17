8fde9360c42e2b1c3b29cb615f8a24e7a76febf5
Chris Chasm <chris@chasm.solutions>
2023-01-17 08:15:15 -0700

    Fix to prayer icon and prayer footer

--- pages/pray/action-custom-map.php
+++ pages/pray/action-custom-map.php
@@ -145,24 +145,16 @@ class PG_Custom_Prayer_App_Map extends PG_Custom_Prayer_App {
                     <div class="map-overlay" id="map-legend"></div>
                     <div class="row">
                         <div class="col col-12 center"><button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas_stats" aria-controls="offcanvas_stats"><i class="ion-chevron-up two-em"></i></button></div>
-
-                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Remaining</strong><br><strong><span class="one-em red-bg stats-figure remaining"></span></strong></div>
-                        <div class="col col-sm-6 col-md-3 center d-none d-md-block"><strong>Places Covered</strong><br><strong><span class="one-em green-bg stats-figure completed"></span></strong></div>
-                         <div class="col col-sm-6 col-md-3 center">
-                            <strong>Prayer Warriors</strong>
-                            <br>
-                            <div class="map-toggle active" data-layer-id="participants">
-                                <img class="foot__icon" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/praying-hand-up-20.png' ) ?>" />
-                            </div>
+                        <div class="col col-sm-6 col-md-3 center "><strong>Places Remaining</strong><br><strong><span class="one-em red-bg stats-figure remaining"></span></strong></div>
+                        <div class="col col-sm-6 col-md-3 center"><strong>Places Covered</strong><br><strong><span class="one-em green-bg stats-figure completed"></span></strong></div>
+                        <div class="col col-sm-6 col-md-3 center d-none d-md-block">
+                            <strong>Warriors</strong><br>
+                            <strong><span class="stats-figure warriors"></span></strong>
                         </div>
-                        <div class="col col-sm-6 col-md-3 center">
-                            <strong>Your Recent Prayers</strong>
-                            <br>
-                            <div class="map-toggle active" data-layer-id="user_locations">
-                                <img class="foot__icon" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/black-check-50.png' ) ?>" />
-                            </div>
+                        <div class="col col-sm-6 col-md-3 center d-none d-md-block">
+                            <strong>Minutes Prayed</strong><br>
+                            <strong><span class="stats-figure minutes_prayed"></span></strong>
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -223,6 +215,20 @@ class PG_Custom_Prayer_App_Map extends PG_Custom_Prayer_App {
                 <div class="col col-6 col-sm-3">
                     <p class="stats-title">Places Covered</p>
                     <p class="stats-figure green-bg completed">0</p>
+                </div>
+                <div class="col col-6 col-sm-3 center">
+                    <strong>Prayer Warriors</strong>
+                    <br>
+                    <div class="map-toggle active" data-layer-id="participants">
+                        <img class="foot__icon" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/avatar1.png' ) ?>" />
+                    </div>
+                </div>
+                <div class="col col-6 col-sm-3 center">
+                    <strong>Your Recent Prayers</strong>
+                    <br>
+                    <div class="map-toggle active" data-layer-id="user_locations">
+                        <img class="foot__icon" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'assets/images/black-check-50.png' ) ?>" />
+                    </div>
                 </div>
                 <div class="col col-6 col-sm-3">
                     <p class="stats-title">Warriors</p>
