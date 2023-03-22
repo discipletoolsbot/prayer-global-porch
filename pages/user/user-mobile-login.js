b68938bb6d92266f23d7bd859bdb01a2f1a8a556
Nath <nathinabob@gmail.com>
2023-03-22 08:35:58 +0000

    style: format code consistently

--- pages/user/user-mobile-login.js
+++ pages/user/user-mobile-login.js
@@ -1,95 +1,102 @@
-jQuery(document).ready(function(){
-    /* We can access all of the top level constants and functions declared in login-shortcodes.php for the login shortcode */
-    const googleButtonSelector = '.firebaseui-idp-google';
-    const facebookButtonSelector = '.firebaseui-idp-facebook';
+jQuery(document).ready(function () {
+  /* We can access all of the top level constants and functions declared in login-shortcodes.php for the login shortcode */
+  const googleButtonSelector = ".firebaseui-idp-google";
+  const facebookButtonSelector = ".firebaseui-idp-facebook";
 
-    console.log(auth)
+  console.log(auth);
 
-    // TODO: remove || true from this line
-    const isGoNative = navigator.userAgent.indexOf("gonative") >= 0
+  // TODO: remove || true from this line
+  const isGoNative = navigator.userAgent.indexOf("gonative") >= 0;
 
-    if (isGoNative) {
-        function initialiseMobileButton(selector, socialProvider, callback) {
-          console.log('initialising google mobile button')
-          const buttonElement = document.querySelector(selector)
+  if (isGoNative) {
+    function initialiseMobileButton(selector, socialProvider, callback) {
+      console.log("initialising google mobile button");
+      const buttonElement = document.querySelector(selector);
 
-          const buttonClone = buttonElement.cloneNode(true)
-          const parentNode = buttonElement.parentNode
+      const buttonClone = buttonElement.cloneNode(true);
+      const parentNode = buttonElement.parentNode;
 
-          buttonElement.remove()
+      buttonElement.remove();
 
-          if ( socialProvider === 'google' ) {
-            buttonClone.onclick = () => gonative.socialLogin.google.login({ 'callback' : callback })
-          } else if ( socialProvider === 'facebook') {
-            buttonClone.onclick = () => gonative.socialLogin.facebook.login({ 'callback' : callback })
-          }
-
-          parentNode.appendChild(buttonClone)
-        }
-
-        waitForElement(googleButtonSelector, () => initialiseMobileButton(
-          googleButtonSelector,
-          'google',
-          providerLoginCallback
-        ))
-        waitForElement(facebookButtonSelector, () => initialiseMobileButton(
-          facebookButtonSelector,
-          'facebook',
-          providerLoginCallback
-        ))
-    }
-
-    function waitForElement(selector, callback) {
-      console.log('waiting for element', selector)
-        const timeIncrement = 200
-
-        const ticker = setInterval(() => {
-            const element = document.querySelector(selector)
-
-            if (!element) return
-
-            clearInterval(ticker)
-
-            callback()
-        }, timeIncrement)
-    }
-
-    function providerLoginCallback(response) {
-      console.log("Google Login Callback response", response);
-
-      let token
-
-      const provider = response.type
-      if ( provider === 'google' ) {
-        token = response.idToken;
-      }
-      if ( provider === 'facebook' ) {
-        token = response.accessToken
+      if (socialProvider === "google") {
+        buttonClone.onclick = () =>
+          gonative.socialLogin.google.login({ callback: callback });
+      } else if (socialProvider === "facebook") {
+        buttonClone.onclick = () =>
+          gonative.socialLogin.facebook.login({ callback: callback });
       }
 
-      if (token) {
-        console.log('we have an idToken', token)
-        const { GoogleAuthProvider, FacebookAuthProvider } = firebase.auth
+      parentNode.appendChild(buttonClone);
+    }
 
-        let credential
+    waitForElement(googleButtonSelector, () =>
+      initialiseMobileButton(
+        googleButtonSelector,
+        "google",
+        providerLoginCallback
+      )
+    );
+    waitForElement(facebookButtonSelector, () =>
+      initialiseMobileButton(
+        facebookButtonSelector,
+        "facebook",
+        providerLoginCallback
+      )
+    );
+  }
 
-        if ( provider === 'google' ) {
-          credential = GoogleAuthProvider.credential(token);
-        }
-        if ( provider === 'facebook' ) {
-          credential = FacebookAuthProvider.credential(token);
-        }
+  function waitForElement(selector, callback) {
+    console.log("waiting for element", selector);
+    const timeIncrement = 200;
 
-        console.log('attempting signIn with credential', credential)
+    const ticker = setInterval(() => {
+      const element = document.querySelector(selector);
 
-        // Sign in with credential from the Google user.
-        auth.signInWithCredential(credential)
+      if (!element) return;
+
+      clearInterval(ticker);
+
+      callback();
+    }, timeIncrement);
+  }
+
+  function providerLoginCallback(response) {
+    console.log("Google Login Callback response", response);
+
+    let token;
+
+    const provider = response.type;
+    if (provider === "google") {
+      token = response.idToken;
+    }
+    if (provider === "facebook") {
+      token = response.accessToken;
+    }
+
+    if (token) {
+      console.log("we have an idToken", token);
+      const { GoogleAuthProvider, FacebookAuthProvider } = firebase.auth;
+
+      let credential;
+
+      if (provider === "google") {
+        credential = GoogleAuthProvider.credential(token);
+      }
+      if (provider === "facebook") {
+        credential = FacebookAuthProvider.credential(token);
+      }
+
+      console.log("attempting signIn with credential", credential);
+
+      // Sign in with credential from the Google user.
+      auth
+        .signInWithCredential(credential)
         .then((authResult) => {
           /* Then we will send *that* token to WP to exchange for a token :O) */
 
-          console.log('signInWithCredential response', authResult)
+          console.log("signInWithCredential response", authResult);
 
-          return signInSuccessWithAuthResult(authResult)
+          return signInSuccessWithAuthResult(authResult);
         })
         .catch((error) => {
           // Handle Errors here.
@@ -98,11 +105,15 @@ jQuery(document).ready(function(){
           // The email of the user's account used.
           const email = error.email;
 
-          console.log('signInWithCredential errors', errorCode, errorMessage, email)
+          console.log(
+            "signInWithCredential errors",
+            errorCode,
+            errorMessage,
+            email
+          );
         });
-
-      } else {
-        console.log("User cancelled login or did not fully authorize.");
-      }
+    } else {
+      console.log("User cancelled login or did not fully authorize.");
     }
-})
+  }
+});
