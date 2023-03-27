9c3cd420126869cf51d625f08fc6c9e3405b1d23
Nath <nathinabob@gmail.com>
2023-03-27 13:05:53 +0100

    ci: add flag into test_phpcs.sh script to stop accidentally running on all files

--- test/test_phpcs.sh
+++ test/test_phpcs.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if [[ $# = 0 ]]; then
+    echo "No arguments given: skipping test"
+    echo "If you meant to run phpcs on the entire codebase then run"
+    echo
+    echo "$0 --test-all"
+    exit 0
+fi
+
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 if [ "$(php -r 'echo version_compare( phpversion(), "7.0", ">=" ) ? 1 : 0;')" != 1 ] ; then
@@ -9,4 +17,9 @@ if [ "$(php -r 'echo version_compare( phpversion(), "7.0", ">=" ) ? 1 : 0;')" !=
     exit
 fi
 
-eval vendor/bin/phpcs $@
+args=$@
+if [[ "$1" = "--test-all" ]]; then
+    args=""
+fi
+
+eval vendor/bin/phpcs $args
