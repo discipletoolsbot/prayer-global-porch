cd5f8c05b0fabfc6e5ae64aca33c07ed0c4e2449
Nath <nathinabob@gmail.com>
2023-04-07 16:00:51 +0100

    chore: Release 1.8.4

--- release.sh
+++ release.sh
@@ -8,14 +8,22 @@ fi
 version=$1
 date=$(date +%F)
 
+## Update package.json
 echo "Updating package.json file to version ${version}"
 
-## Update package.json
 sed -i "s/\"version\": \".*\"/\"version\": \"${version}\"/" package.json
 
-echo "Updating version-control.json to version ${version} and last updated to ${date}"
 ## Update version-control.json
+echo "Updating version-control.json to version ${version} and last updated to ${date}"
+
 sed -i "s/\"version\": \".*\"/\"version\": \"${version}\"/" version-control.json
 sed -i "s/\"last_updated\": \".*\"/\"last_updated\": \"$date\"/" version-control.json
 
 export PG_VERSION=$version
+
+echo 'Pushing new tag to master'
+
+git add .
+git commit -m "chore: Release ${version}"
+git tag ${version}
+git push --tags origin master
