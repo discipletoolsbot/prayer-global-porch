2306d94c68e7cfe24800433ea7de6127f5fccdea
Nath <nathinabob@gmail.com>
2023-04-07 16:20:41 +0100

    ci: update plugin version in plugin root file as well

--- release.sh
+++ release.sh
@@ -8,22 +8,18 @@ fi
 version=$1
 date=$(date +%F)
 
-## Update package.json
 echo "Updating package.json file to version ${version}"
-
 sed -i "s/\"version\": \".*\"/\"version\": \"${version}\"/" package.json
 
-## Update version-control.json
 echo "Updating version-control.json to version ${version} and last updated to ${date}"
-
 sed -i "s/\"version\": \".*\"/\"version\": \"${version}\"/" version-control.json
 sed -i "s/\"last_updated\": \".*\"/\"last_updated\": \"$date\"/" version-control.json
 
-export PG_VERSION=$version
+echo "Updating prayer-global-porch.php to version ${version}"
+sed -i "s/\"Version\":.*/\"Version\": ${version}/" prayer-global-porch.php
 
 echo 'Pushing new tag to master'
-
 git add .
-git commit -m "chore: Release ${version}"
+git commit -m "Release ${version}"
 git tag ${version}
 git push --tags origin master
