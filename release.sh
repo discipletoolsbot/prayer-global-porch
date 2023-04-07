4622016fda9c7ea5e9a924b59a11e3395c4c9048
Nath <nathinabob@gmail.com>
2023-04-07 15:56:16 +0100

    fix: use version env variable and create new tag

--- release.sh
+++ release.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo $#
-
 if [[ $# != 1 ]]; then
     echo 'Error: you must specify a version number'
     exit 1
 fi
 
 version=$1
+date=$(date +%F)
+
+echo "Updating package.json file to version ${version}"
 
 ## Update package.json
 sed -i "s/\"version\": \".*\"/\"version\": \"${version}\"/" package.json
 
+echo "Updating version-control.json to version ${version} and last updated to ${date}"
 ## Update version-control.json
 sed -i "s/\"version\": \".*\"/\"version\": \"${version}\"/" version-control.json
-
-date=$(date +%F)
-
 sed -i "s/\"last_updated\": \".*\"/\"last_updated\": \"$date\"/" version-control.json
+
+export PG_VERSION=$version
