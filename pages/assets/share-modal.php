79b53dedd242cfecedd8ebf9733d30a24ae297dc
Nath <nathinabob@gmail.com>
2023-04-26 09:31:53 +0100

    feat: make share modal translateable

--- pages/assets/share-modal.php
+++ pages/assets/share-modal.php
@@ -3,7 +3,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="share-modal-label">Share Prayer.Global</h5>
+        <h5 class="modal-title" id="share-modal-label"><?php echo esc_html__( 'Share Prayer.Global', 'prayer-global-porch' ) ?></h5>
       </div>
       <div class="modal-body ">
         <ul class="list-group list-group-flush share-modal__items">
@@ -14,10 +14,12 @@
                 <img class="share__icon" src="<?php echo esc_html( plugin_dir_url( __FILE__ ) ) ?>/images/twitter.svg" alt="Share">Twitter
             </li>
             <li class="list-group-item list-group-item-action list-group-item-secondary email-action">
-                <img class="share__icon" src="<?php echo esc_html( plugin_dir_url( __FILE__ ) ) ?>/images/email.svg" alt="Share">Email
+                <img class="share__icon" src="<?php echo esc_html( plugin_dir_url( __FILE__ ) ) ?>/images/email.svg" alt="Share">
+                <?php echo esc_html( __( 'Email', 'prayer-global' ) ) ?>
             </li>
             <li class="list-group-item list-group-item-action list-group-item-secondary link-action">
-                <img class="share__icon" src="<?php echo esc_html( plugin_dir_url( __FILE__ ) ) ?>/images/link.svg" alt="Share">Link <span class="copy-notice">Copied</span>
+                <img class="share__icon" src="<?php echo esc_html( plugin_dir_url( __FILE__ ) ) ?>/images/link.svg" alt="Share">
+                <?php echo wp_kses( __( 'Link <span class="copy-notice">Copied</span>', 'prayer-global' ), 'post' ) ?>
             </li>
         </ul>
       </div>
