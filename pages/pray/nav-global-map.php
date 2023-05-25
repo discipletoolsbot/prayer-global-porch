39136edc3436b8ad504d8e664c3f6be3d6bef828
Nath <nathinabob@gmail.com>
2023-05-25 16:20:44 +0100

    feat: add pg icons to the global map nav bar

--- pages/pray/nav-global-map.php
+++ pages/pray/nav-global-map.php
@@ -1,10 +1,15 @@
 <nav class="navbar-dark bg-none p-0" id="pg-navbar">
-    <div class="d-flex align-items-center justify-space-between mx-0 px-0 mw-100 flex-nowrap">
-        <a href="/" class="col icon-button two-rem" title="Home"><i class="ion-home"></i></a>
+    <div class="d-flex align-items-center justify-content-between mx-0 px-0 mw-100 flex-nowrap">
+        <a href="/" class="icon-button two-rem d-flex align-items-center mx-2" title="Home">
+            <i class="icon pg-home"></i>
+        </a>
 
-        <div class="col-8 col-md-4 d-flex justify-content-end">
-            <a class="btn btn-cta py-2 me-3" href="/newest/lap/">Start Praying</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#probootstrap-navbar" aria-controls="probootstrap-navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="d-flex justify-content-end align-items-center">
+            <div><a class="btn btn-cta mx-2" href="/newest/lap/">Start Praying</a></div>
+            <a href="/user_app/profile" class="icon-button mx-2 two-rem d-flex align-items-center" title="Profile" id="user-profile-link">
+                <i class="icon pg-profile"></i>
+            </a>
+            <button class="navbar-toggler mx-2 two-rem" type="button" data-bs-toggle="offcanvas" data-bs-target="#probootstrap-navbar" aria-controls="probootstrap-navbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span><i class="ion-navicon"></i></span>
             </button>
         </div>
