61a697530c1b7f1e4b0bee30a76dd776afb43d6a
Nath <nathinabob@gmail.com>
2023-06-15 18:01:34 +0100

    feat: restlye the footer

--- pages/assets/working-footer.php
+++ pages/assets/working-footer.php
@@ -1,37 +1,30 @@
-<footer class="pg-footer" role="contentinfo"  style="background-image:  linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/1900x1200_img_2.jpg);">
+<footer class="brand-bg white center one-rem font-weight-bold py-5 pg-footer" role="contentinfo">
     <div class="container">
-        <div class="row" >
-            <div class="col-md-3">
-                <p class="small">
-                    Made with <a href="/user_app/login">&#10084;&#65039;</a> by <a href="https://gospelambition.org" style="color:black;">Gospel Ambition</a>.<br>
-                    Powered by <a href="https://disciple.tools" style="color:black;">Disciple.Tools</a>.<br>
-                    Part of the <a href="https://pray4movement.org" style="color:black;">Pray4Movement network</a>.
-                </p>
-            </div>
-            <div class="col-md-3">
-                <ul style="list-style: none;">
-                    <li><a class="white" href="https://apps.apple.com/us/app/prayer-global/id1636889534?uo=4"><i class="ion-social-apple"></i> iPhone/iPad App</a></li>
-                    <li><a class="white" href="https://play.google.com/store/apps/details?id=app.global.prayer"><i class="ion-social-android"></i> Android App</a></li>
-                </ul>
-            </div>
-            <div class="col-md-3">
-                <ul style="list-style: none;">
-                    <li><a class="white" href="/prayer_app/contact_us/">Contact Us</a></li>
-                    <li><a class="white fw-bold" href="/content_app/give_page">Give</a></li>
-                </ul>
-            </div>
-            <div class="col-md-3">
-                <ul style="list-style: none;">
-                    <li><a class="white" href="/content_app/about_page/">About</a></li>
-                    <li><a class="white" href="/content_app/data_sources/">Data Sources</a></li>
-                    <li><a class="white" href="/download_app/media/">Media & Promotion</a></li>
-                </ul>
-            </div>
-        </div>
+        <p>
+            Made with
+            <a href="/user_app/login">
+                <svg class="pg-heart" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>heart</title> <path d="M0.256 12.16q0.544 2.080 2.080 3.616l13.664 14.144 13.664-14.144q1.536-1.536 2.080-3.616t0-4.128-2.080-3.584-3.584-2.080-4.16 0-3.584 2.080l-2.336 2.816-2.336-2.816q-1.536-1.536-3.584-2.080t-4.128 0-3.616 2.080-2.080 3.584 0 4.128z"></path> </g></svg>
+            </a> by <a href="https://gospelambition.org">Gospel Ambition</a>.<br>
+            Powered by <a href="https://disciple.tools">Disciple.Tools</a>.<br>
+            Part of the <a href="https://pray4movement.org">Pray4Movement network</a>.
+        </p>
+        <ul style="list-style: none;">
+            <li><a class="white" href="https://apps.apple.com/us/app/prayer-global/id1636889534?uo=4"><i class="ion-social-apple"></i> iPhone/iPad App</a></li>
+            <li><a class="white" href="https://play.google.com/store/apps/details?id=app.global.prayer"><i class="ion-social-android"></i> Android App</a></li>
+        </ul>
+        <ul style="list-style: none;">
+            <li><a class="white" href="/prayer_app/contact_us/">Contact Us</a></li>
+            <li><a class="white fw-bold" href="/content_app/give_page">Give</a></li>
+        </ul>
+        <ul style="list-style: none;">
+            <li><a class="white" href="/content_app/about_page/">About</a></li>
+            <li><a class="white" href="/content_app/data_sources/">Data Sources</a></li>
+            <li><a class="white" href="/download_app/media/">Media & Promotion</a></li>
+        </ul>
 
-        <div class="row" style="padding-top: 5rem;">
+        <div class="row pt-4">
             <div class="col text-center">
-                <p class="small">Prayer Global &copy; <script>document.write(new Date().getFullYear())</script> Gospel Ambition</p>
+                <p>Prayer Global &copy; <script>document.write(new Date().getFullYear())</script> Gospel Ambition</p>
             </div>
         </div>
     </div>
