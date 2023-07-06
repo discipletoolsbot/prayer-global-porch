b3a1d1a9c71944254fa142bdda73eb608502da5e
Nath <nathinabob@gmail.com>
2023-07-06 18:47:20 +0100

    feat: add desktop story structure and some basic styling

--- pages/home/body.php
+++ pages/home/body.php
@@ -23,7 +23,79 @@
 </section>
 <!-- END section -->
 
-<section class="page-section full-height" id="section-goal">
+<section class="page-section section-goal-desktop lh-base" id="section-goal">
+
+
+    <div class="position-relative">
+        <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/desktop-story-background-01.png" alt="">
+        <div class="white desktop-story__container text-left container flow-small">
+            <h2 class="font-title"><i class="icon pg-logo-prayer me-3"></i><?php echo esc_html__( 'The Goal', 'prayer-global' ) ?></h2>
+            <p>
+                <?php echo sprintf( esc_html__( 'The clock is ticking. %sPrayer.Global seeks to encourage extraordinary prayer for the fulﬁllment of the Great Commission in our generation.', 'prayer-global-porch' ), '</p><p>' ) ?>
+            </p>
+            <p>
+                <?php echo esc_html__( 'But how?', 'prayer-global-porch' ) ?>
+            </p>
+        </div>
+    </div>
+    <div class="position-relative">
+        <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/desktop-story-background-02.png" alt="">
+        <div class="brand-light desktop-story__container text-right-40 container flow-small text-align-right">
+            <h2 class="font-title"><?php echo esc_html__( 'The Race', 'prayer-global' ) ?><i class="icon pg-relay ms-3"></i></h2>
+            <p>
+                <?php echo esc_html__( 'Like a race, we pray with urgency and focus as we seek to complete laps by covering the entire world in prayer. An interactive map gives us real time updates on who we’ve prayed for and stats on our collective prayer lap status.', 'prayer-global-porch' ) ?>
+            </p>
+        </div>
+    </div>
+    <div class="position-relative">
+        <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/desktop-story-background-03.png" alt="">
+        <div class="brand-light desktop-story__container text-left container flow-small">
+            <h2 class="font-title"><i class="icon pg-world-arrow me-3"></i><?php echo esc_html__( 'Our Strategy', 'prayer-global' ) ?></h2>
+            <p>
+                <?php echo esc_html__( 'Prayer.Global has broken the world down into 4,770 states based on geographical and governmental boundaries. With location speciﬁc prayer fuel, we can pray with purpose for every people group and watch on the map as our united prayer lights up the darkness.', 'prayer-global-porch' ) ?>
+            </p>
+        </div>
+    </div>
+    <div class="position-relative">
+        <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/desktop-story-background-04.png" alt="">
+        <div class="brand-light desktop-story__container text-right-40 container center">
+            <h2 class="font-title mb-3"><?php echo esc_html__( 'Current Status', 'prayer-global' ) ?><i class="icon pg-logo-prayer ms-3"></i></h2>
+            <div class="brand-lighter-bg white rounded-4 mx-auto mb-2 w-fit pt-4 pb-2 px-3 pb-0">
+                <h3 class="font-base uppercase font-weight-bold" style="line-height: 0.7"><?php echo sprintf( esc_html_x( 'Prayer Laps %s', 'Prayer Laps Completed', 'prayer-global' ), sprintf( '<br> <span class="one-rem">%s</span>', esc_html_x( 'Completed', 'Laps Completed', 'prayer-global-porch' ) ) ) ?></h3>
+                <h3 class="global-laps-completed six-em lh-xsm"><span class="loading-spinner active"></span></h3>
+            </div>
+            <p class="white-gradient pb-3 mx-auto">
+                <?php echo sprintf( esc_html__( 'Our ﬁrst global race started %1$s days ago and we’ve completed %2$s laps together so far.', 'prayer-global-porch' ), '<span class="global-days-elapsed"><span class="loading-spinner active"></span></span>', '<span class="global-laps-completed"><span class="loading-spinner active"></span></span>' ) ?>
+            </p>
+            <div class="">
+                <h3 class="font-base font-weight-bold uppercase"><?php echo sprintf( esc_html__( 'Lap %s Time Elapsed', 'prayer-global' ), '<span class="global-lap-number"><span class="loading-spinner active"></span></span>' ) ?></h3>
+                <div class="white brand-bg rounded-4 m-auto w-fit px-4 py-3 two-em font-weight-bold uppercase | current-time-elapsed time_elapsed">
+                    <span class="loading-spinner active"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="position-relative">
+        <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/desktop-story-background-05.png" alt="">
+        <div class="brand-light desktop-story__container text-right-50 text-bottom container flow-small text-align-right me-4">
+            <p class="pb-3">
+                <?php echo esc_html__( 'With each lap we complete, we are that much closer to ushering in God’s Kingdom and the return of Jesus Christ! Step up and join the worldwide Prayer.Global prayer team!', 'prayer-global-porch' ) ?>
+            </p>
+            <div class="center align-self-end">
+                <h2 class="font-title"><?php echo esc_html__( 'Ready. Set.', 'prayer-global' ) ?></h2>
+                <div class="mb-2"><a class="btn btn-cta mx-2 two-rem" href="/newest/lap/"><?php echo esc_html__( 'Start Praying', 'prayer-global' ) ?></a></div>
+                <i class="icon pg-logo-prayer icon-medium d-block"></i>
+            </div>
+        </div>
+        <div class="white desktop-story__container text-left text-bottom container center mt-5">
+            <h2 class="font-title mb-0">Prayer.Global</h2>
+            <h3>Cover The World In Prayer</h3>
+        </div>
+    </div>
+
+</section>
+
+<section class="page-section full-height section-goal-mobile" id="section-goal">
     <div class="container">
         <div class="row text-center justify-content-center">
             <div id="storyCarousel" class="carousel slide px-0">
@@ -35,7 +107,7 @@
                                     <i class="icon pg-logo-prayer icon-medium my-3 d-block"></i>
                                     <h2 class="font-title mb-3"><?php echo esc_html__( 'The Goal', 'prayer-global' ) ?></h2>
                                     <p class="white-gradient pb-3">
-                                        <?php echo esc_html__( 'The clock is ticking. Prayer.Global seeks to encourage extraordinary prayer for the fulﬁllment of the Great Commission in our generation.', 'prayer-global-porch' ) ?>
+                                        <?php echo sprintf( esc_html__( 'The clock is ticking. %sPrayer.Global seeks to encourage extraordinary prayer for the fulﬁllment of the Great Commission in our generation.', 'prayer-global-porch' ), '' ) ?>
                                         <br>
                                         <?php echo esc_html__( 'But how?', 'prayer-global-porch' ) ?>
                                     </p>
