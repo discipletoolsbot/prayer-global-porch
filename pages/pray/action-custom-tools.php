d9ef8836c1067e5a9ac33e1fef4fe50412488799
Nath <nathinabob@gmail.com>
2023-07-07 17:45:48 +0100

    fix: reduce width of QR codes on share page

--- pages/pray/action-custom-tools.php
+++ pages/pray/action-custom-tools.php
@@ -89,8 +89,8 @@ class PG_Custom_Prayer_App_Tools extends PG_Custom_Prayer_App {
                         <h3><?php echo esc_html( sprintf( __( '%s Relay Map', 'prayer-global-porch' ), $lap_stats['title'] ) ) ?></h3>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col center">
+                <div class="row justify-content-center">
+                    <div class="col col-6 col-md-4 col-lg-3 center">
                         <img src="https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&amp;data=<?php echo esc_url( get_site_url() ) ?>/prayer_app/custom/<?php echo esc_html( $lap_stats['key'] ) ?>/map" style="width: 100%;max-width:400px;"><br><br>
                     </div>
                 </div>
@@ -136,20 +136,21 @@ class PG_Custom_Prayer_App_Tools extends PG_Custom_Prayer_App {
                         <h3><?php echo esc_html( sprintf( __( 'App Stores: %s App', 'prayer-global-porch' ), 'Prayer.Global' ) ) ?></h3>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col center">
+                <div class="row justify-content-center">
+                    <div class="col col-6 col-md-4 col-lg-3 center">
                         <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) . 'assets/images/prayer.global.app.png' ) ?>" style="width: 100%;max-width:400px;"><br><br>
-                        <div class="input-group">
-                            <input type="text" class="form-control copy-input"
-                                   value="<?php echo esc_url( get_site_url() ) ?>/qr/app" placeholder="Some path" id="copy-input-qrapp">
-                          <button class="btn btn-secondary copy-button input-group-btn" type="button" id="copy-button-qrapp"
-                                  data-bs-toggle="tooltip" data-placement="button"
-                                  title="<?php esc_attr( __( 'Copy to Clipboard', 'prayer-global-porch' ) ) ?>" data-clipboard-action="copy" data-clipboard-target="#copy-input-qrapp">
-                            <?php echo esc_html( __( 'Copy', 'prayer-global-porch' ) ) ?>
-                          </button>
-                        </div>
                     </div>
                 </div>
+                <div class="input-group">
+                    <input type="text" class="form-control copy-input"
+                           value="<?php echo esc_url( get_site_url() ) ?>/qr/app" placeholder="Some path" id="copy-input-qrapp">
+                  <button class="btn btn-secondary copy-button input-group-btn" type="button" id="copy-button-qrapp"
+                          data-bs-toggle="tooltip" data-placement="button"
+                          title="<?php esc_attr( __( 'Copy to Clipboard', 'prayer-global-porch' ) ) ?>" data-clipboard-action="copy" data-clipboard-target="#copy-input-qrapp">
+                    <?php echo esc_html( __( 'Copy', 'prayer-global-porch' ) ) ?>
+                  </button>
+                </div>
+
 
             </div>
 
