a4db1b00f8ec6e225593e7a3892becee2b01fba7
Nath <nathinabob@gmail.com>
2023-07-11 08:15:04 +0100

    build: update release script to auto increment version based on options

--- release.sh
+++ release.sh
@@ -1,11 +1,53 @@
 #!/bin/bash
 
-if [[ $# != 1 ]]; then
-    echo 'Error: you must specify a version number'
-    exit 1
+TEMP=`getopt -o Mmf --long major,minor,fix -- "$@"`
+eval set -- "$TEMP"
+
+while true; do
+    case "$1" in
+        -M|--major)
+            type='major'; shift;;
+        -m|--minor)
+            type='minor'; shift;;
+        -f|--fix)
+            type='fix'; shift;;
+        --)
+            shift; break;;
+    esac
+done
+
+currentVersion=$(grep version package.json | grep -oP '\d*\.\d*\.\d*')
+
+IFS='.' read -ra ADDR <<< "$currentVersion"
+major=${ADDR[0]}
+minor=${ADDR[1]}
+fix=${ADDR[2]}
+
+if [[ "$type" = "" ]]; then
+    version=$1
+else
+    if [[ "$type" = 'major' ]]; then
+        major=$(( major + 1 ))
+        minor=0
+        fix=0
+    fi
+    if [[ "$type" = 'minor' ]]; then
+        minor=$(( minor + 1 ))
+        fix=0
+    fi
+    if [[ "$type" = 'fix' ]]; then
+        fix=$(( fix + 1 ))
+    fi
+
+    version="$major.$minor.$fix"
 fi
 
-version=$1
+read -p "Release version $version? (Y/n) " answer
+
+if [[ "$answer" = "n" ]]; then
+    exit 0
+fi
+
 date=$(date +%F)
 
 echo "Updating package.json file to version ${version}"
