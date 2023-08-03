6ac015fa2117c63b948e9e1a958b0053e6add12e
Nath <nathinabob@gmail.com>
2023-08-03 14:00:15 +0100

    fix: enable ctas on the global map

--- pages/pray/action-global-map.php
+++ pages/pray/action-global-map.php
@@ -101,6 +101,7 @@ class PG_Global_Prayer_App_Map extends PG_Global_Prayer_App {
                     'add' => __( 'Add Magic', 'prayer-global-porch' ),
                 ],
                 'map_type' => 'binary',
+                'is_cta_feature_on' => true,
             ]) ?>][0]
         </script>
         <link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,600|Montserrat:200,300,400" rel="stylesheet">
