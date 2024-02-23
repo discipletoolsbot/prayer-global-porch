b9bfdc3ad3f8c2042be072d4b2f03bddca474f01
Nath <nathinabob@gmail.com>
2024-02-23 16:09:23 +0000

    feat: add how to do PG event video to media page

--- pages/media/magic-media.php
+++ pages/media/magic-media.php
@@ -87,7 +87,7 @@ class Prayer_Global_Porch_Media extends DT_Magic_Url_Base
                         <h2 class="mt-0 font-weight-normal"><?php echo esc_html( __( 'Media & Promotion', 'prayer-global-porch' ) ) ?></h2>
                     </div>
                 </div>
-                <div class="row justify-content-md-center text-center mb-5" style="display: none">
+                <div class="row justify-content-md-center text-center mb-5">
                     <div class="col-12 mb-3">
                        <hr>
                     </div>
@@ -96,21 +96,23 @@ class Prayer_Global_Porch_Media extends DT_Magic_Url_Base
                     </div>
                     <div class="col-lg-3 p-3">
                         <?php echo esc_html( __( 'Video', 'prayer-global-porch' ) ) ?><br>
-                        <div style="padding:56.3% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/715752828?h=d39d43cea8&amp;badge=0&amp;autopause=1&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="moravian-prayer-challenge"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+                        <div style="padding:56.3% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/913555379?h=b79d3f9229&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
                     </div>
                     <div class="col-lg-3 p-3">
                         <p>
-                            <?php echo esc_html( __( 'Download Video', 'prayer-global-porch' ) ) ?>
-                            <br><a href="https://player.vimeo.com/progressive_redirect/download/715752828/container/44c88492-73ba-4789-9e48-c88316194653/c2189175/moravian-prayer-challenge%20%28360p%29.mp4?expires=1656520543&loc=external&signature=35931e268af8c01f4c82c771b47d2b3393fca9f000dc62ff89afe0f6bb6ac1a5"><?php echo esc_html__( 'Low Quality (360p - 10mg)', 'prayer-global-porch' ) ?></a>
-                            <br><a href="https://player.vimeo.com/progressive_redirect/download/715752828/container/44c88492-73ba-4789-9e48-c88316194653/19ce9b8c-bc019d05/moravian-prayer-challenge%20%281080p%29.mp4?expires=1656520543&loc=external&signature=5ee3781d3107607bbb25b556a0c87cacfa4126412d3c2e9412b2db2f9366c18d"><?php echo esc_html__( 'HD Download (1080p - 31mg)', 'prayer-global-porch' ) ?></a>
-                            <br><a href="https://player.vimeo.com/progressive_redirect/download/715752828/container/44c88492-73ba-4789-9e48-c88316194653/538db78d/moravian-prayer-challenge%20%282160p%29.mp4?expires=1656520543&loc=external&signature=2e0906096aec000bd78276f5bbde85f9e9c9ad7d627c4c92bfa14e58335faea9"><?php echo esc_html__( '4k Download (2160p - 74mg)', 'prayer-global-porch' ) ?></a>
+                            <?php echo esc_html( __( 'Embed Code', 'prayer-global-porch' ) ) ?><br>
+                            <textarea style="font-size: .6em; height: 150px; width: 100%;" readonly><div style="padding:56.3% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/913555379?h=b79d3f9229&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script></textarea>
                         </p>
                     </div>
                     <div class="col-lg-3 p-3">
-                        <h3>Resources</h3>
+                        <?php echo esc_html( __( 'Resources', 'prayer-global-porch' ) ) ?><br>
                         <p>
-                            <a href="/church">How to launch Prayer.Global at your church</a>
+                            <a href="/church"><?php echo esc_html__( 'How to launch Prayer.Global at your church document', 'prayer-global-porch' ) ?></a>
                         </p>
+                        <p>
+                            <a href="https://vimeo.com/913555379" target="_blank"><?php echo esc_html__( 'Wayne Bushnell Prayer.Global Testimony Video', 'prayer-global-porch' ) ?> <i class="ion-android-open"></i></a>
+                        </p>
+
                     </div>
                 </div>
                 <div class="row justify-content-md-center text-center mb-5">
