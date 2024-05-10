1e478eeb7d3d29938e277614e51987335e90ff04
corsacca <corsacca@gmail.com>
2024-05-10 16:48:46 +0100

    Add Umami tracking (#349)
    * Add Umami tracking
    
    * fix: use updated website id
    
    ---------
    
    Co-authored-by: Nath <nathinabob@gmail.com>

--- pages/assets/footer.php
+++ pages/assets/footer.php
@@ -1,1 +1,2 @@
 <?php require_once( __DIR__ . '/share-modal.php' ) ?>
+<script defer src="https://umami.gospelambition.com/script.js" data-website-id="c8b2d630-e64a-4354-b03a-f92ac853153e"></script>
